e79d5e208a60edbed83cad9eb7804656d55e04e3
FattahulAlim <fathalim1512@gmail.com>
2025-04-21 15:09:59 +0700

    first commit

---
+++ SourceCode/Mahasiswa.java
@@ -0,0 +1,18 @@
+public class Mahasiswa {
+    String nim;
+    String namaMahasiswa;
+
+    public Mahasiswa(){
+
+    }
+
+    public Mahasiswa(String nim, String nm){
+        this.nim = nim;
+        namaMahasiswa = nm;
+    }
+
+    void tampilData(){
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM:    " + nim + " |   Nama:   " + namaMahasiswa);
+    }
+}
---
+++ SourceCode/MataKuliah.java
@@ -0,0 +1,20 @@
+public class MataKuliah {
+    String kodeMK;
+    String namaMK;
+    int sks;
+
+    public MataKuliah(){
+
+    }
+
+    public MataKuliah(String kd, String nm, int sks){
+        kodeMK = kd;
+        namaMK = nm;
+        this.sks = sks;
+    }
+
+    void tampilData(){
+        System.out.println("=== DATA MATA KULIAH ===");
+        System.out.println("Kode MK:    " + kodeMK + "  |   Nama:   " + namaMK + "  |   SKS:    " + sks);
+    }
+}
